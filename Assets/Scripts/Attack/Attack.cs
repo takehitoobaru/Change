45113272b0eda10108d597724c96620bc0f9711e
MODOKI\baruo <obaru.namu1101@gmail.com>
2023-11-09 18:49:21 +0900

    [add]攻撃用スクリプト追加

---
+++ Assets/Scripts/Attack/Attack.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    #region property
+    #endregion
+
+    #region serialize
+    #endregion
+
+    #region private
+    #endregion
+
+    #region Constant
+    #endregion
+
+    #region Event
+    #endregion
+
+    #region unity methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnParticleSystemStopped()
+    {
+        
+    }
+    #endregion
+
+    #region public method
+    #endregion
+
+    #region private method
+    #endregion
+}
