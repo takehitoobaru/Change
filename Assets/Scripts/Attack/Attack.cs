8da4c8f75c6feac57b7b0a8e30da0d0d7a2a42c2
MODOKI\baruo <obaru.namu1101@gmail.com>
2023-11-13 18:45:06 +0900

    [update]プールに戻る処理追加

--- Assets/Scripts/Attack/Attack.cs
+++ Assets/Scripts/Attack/Attack.cs
@@ -4,46 +4,8 @@ using UnityEngine;
 
 public class Attack : MonoBehaviour
 {
-    #region property
-    #endregion
-
-    #region serialize
-    #endregion
-
-    #region private
-    #endregion
-
-    #region Constant
-    #endregion
-
-    #region Event
-    #endregion
-
-    #region unity methods
-    private void Awake()
-    {
-
-    }
-
-    private void Start()
-    {
-
-    }
-
-    private void Update()
-    {
-
-    }
-
     private void OnParticleSystemStopped()
     {
-        
+        ObjectPool.Instance.ReleaseGameObject(gameObject);
     }
-    #endregion
-
-    #region public method
-    #endregion
-
-    #region private method
-    #endregion
 }
