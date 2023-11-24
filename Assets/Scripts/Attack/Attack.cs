0928beb00d78c5512e039742dde3589c5d5f3391
MODOKI\baruo <obaru.namu1101@gmail.com>
2023-11-24 19:03:48 +0900

    [update]与ダメ処理追加

--- Assets/Scripts/Attack/Attack.cs
+++ Assets/Scripts/Attack/Attack.cs
@@ -7,8 +7,39 @@ using UnityEngine;
 /// </summary>
 public class Attack : MonoBehaviour
 {
+    [Tooltip("攻撃力")]
+    [SerializeField]
+    private int _attackAmount = 10;
+
+    [Tooltip("属性")]
+    [SerializeField]
+    private Element _attackElement = Element.Fire;
+
+    [Tooltip("キャラクタータイプ")]
+    [SerializeField]
+    private AttackType _charaType = AttackType.Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy") && _charaType == AttackType.Player)
+        {
+            other.gameObject.GetComponent<IDamagable>().Damage(_attackAmount, _attackElement);
+        }
+
+        if (other.CompareTag("Player") && _charaType == AttackType.Enemy)
+        {
+            other.gameObject.GetComponent<IDamagable>().Damage(_attackAmount, _attackElement);
+        }
+    }
+
     private void OnParticleSystemStopped()
     {
         ObjectPool.Instance.ReleaseGameObject(gameObject);
     }
+}
+
+public enum AttackType
+{
+    Player,
+    Enemy
 }
